765615aff26e3d392fe0ccbe8da2bf30abc6f8c9
Kamila-Irshad <kamilairshad366@gmail.com>
2025-10-25 19:22:55 +0500

    made check list

--- src/Components/CheckList.jsx
+++ src/Components/CheckList.jsx
@@ -1,33 +1,45 @@
-"use client"
+"use client";
+import { useState } from "react";
 import UlList from "../Components/ULlist";
 const CheckList = () => {
-   const handleClick = (event) =>{
+  const [inputValue, setInputValue] = useState("");
+  const [task, setTask] = useState([]);
+  const handleInputChange = (value) => {
+    setInputValue(value);
+  };
+  const handleClick = (event) => {
+    event.preventDefault();
 
-    let task = event.target.taskList.value;
-   
-        
-    alert(task);
+    if (!inputValue.includes(inputValue)) return;
 
+    setTask([...task, inputValue]);
 
-    event.preventDefault()
-   }
-
-    return (
-        <>
-        <div className="text-center items-center">
-<form action=""  
-onSubmit={handleClick}
-className="items-center text-center ">
-    <input type="text" 
-        name='taskList'
-    className="p-2 border bg-gray-400"/>
-    <button 
-    className="bg-gray-400 border p-2 mx-2">Save</button>
-    
-</form>
-<UlList></UlList>
-</div>
-        </>
-    )
-}
+    setInputValue("");
+  };
+  return (
+    <>
+      <div className="text-center items-center m-auto  bg-blue-200 w-1/2 justify-center ">
+        <form
+          action=""
+          onSubmit={handleClick}
+          className="items-center text-center m-2  flex flex-row justify-center gap-5"
+        >
+          <input
+            type="text"
+            name="taskList"
+            value={inputValue}
+            onChange={(event) => handleInputChange(event.target.value)}
+            className="p-2 w-lvh border bg-blue-400"
+          />
+          <div>
+            <button className="bg-blue-300 border py-1 rounded text-sm m-2">
+              Add Task{" "}
+            </button>
+          </div>
+        </form>
+        <UlList tasks={task}></UlList>
+      </div>
+    </>
+  );
+};
 export default CheckList;
--- src/Components/ULlist.jsx
+++ src/Components/ULlist.jsx
@@ -1,11 +1,45 @@
 "use client";
-const UlList = () =>{
-    return(
-        <>
-        <ul className="w-full">
-            <li>HTML <span className="p-2"> &times; </span></li>
+const UlList = ({ tasks }) => {
+  return (
+    <>
+      {tasks.map((task, index) => (
+        <ul 
+                    key={index}
+
+        className="w-full flex flex-col text-center 
+         ">
+          <li
+            className="  bg-blue-100 flex text-3xl  justify-between flex-row"
+          >
+            {task}
+            <span className="p-2 text-3xl"> &times; </span>
+          </li>
         </ul>
-        </>
-    )
-}
+      ))}
+    </>
+  );
+};
 export default UlList;
+
+
+// "use client";
+
+// const UlList = ({ tasks }) => {
+//   return (
+//     <>
+//       {tasks.map((task, index) => (
+//         <ul
+//           key={index}
+//           className="w-full flex flex-col text-center"
+//         >
+//           <li className="bg-blue-100 flex text-3xl justify-between flex-row p-2 m-1 rounded">
+//             {task}
+//             <span className="p-2 text-3xl cursor-pointer">&times;</span>
+//           </li>
+//         </ul>
+//       ))}
+//     </>
+//   );
+// };
+
+// export default UlList;
